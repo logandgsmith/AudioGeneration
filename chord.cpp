e54761c648529e9378cdf87c4f9dd3cad5e5e5d6
Damien N Bobrek <dbobrek@ufl.edu>
2018-03-26 22:31:05 -0400

    Actually fixed the chord name issue

---
+++ chord.cpp
@@ -0,0 +1,27 @@
+#include "Chord.h"
+#include "Note_Generator.h"
+
+//Constructor
+Chord :: Chord(std::string name, std::string note_one, std::string note_two, std::string note_three) {
+	this->name       = name;
+	this->note_one   = note_one;
+	this->note_two   = note_two;
+	this->note_three = note_three;
+}
+
+//Destructor
+Chord :: ~Chord() {
+	//FILL IN
+}
+
+std::string Chord::get_note_one() {
+	return this->note_one;
+}
+
+std::string Chord::get_note_two() {
+	return this->note_two;
+}
+
+std::string Chord::get_note_three() {
+	return this->note_three;
+}
