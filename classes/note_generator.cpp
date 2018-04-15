eca5ca71a69d56299d9c593489dd824b5426be2b
Damien N Bobrek <dbobrek@ufl.edu>
2018-04-15 16:57:07 -0400

    Added new functionaity for use in song_writer & made the code compilable

--- classes/note_generator.cpp
+++ classes/note_generator.cpp
@@ -1,25 +1,32 @@
-#include "note_generator.h"
+#include "../headers/note_generator.h"
+#include <algorithm>
+#include <vector>
+#include <ctime> // To be used for note generator
 
-// constructor defaults to middle C (C4)
-NoteGenerator :: NoteGenerator() {
-	
+// constructor defaults to the note range starting one octave below middle C (C3) and being 2 octaves
+NoteGenerator::NoteGenerator() {
+	NoteGenerator::setNote(130.81, std::string("C3"), default_keyboard_size);
 }
 
 //frequncy getter
 double NoteGenerator::getNoteFreq(int index){
-	return keyboard[index].note_frequency;
+	return keyboard.at(index).note_frequency;
 }
 //name getter
 std::string NoteGenerator::getNoteName(int index) {
-	return keyboard[index].note_name;
+	return keyboard.at(index).note_name;
 }
-//array getter
-Note* NoteGenerator::getArray() {
-	return this->keyboard;
+//keyboard vector getter
+std::vector<Note> NoteGenerator::getKeyboard() {
+	return keyboard;
+}
+//keyboard size getter
+size_t NoteGenerator::getKeyboardSize() {
+	return this->keyboard.size();
 }
 
 //Mutators  //ERROR MIGHT OCCUR DUE TO ME NOT USING POINTER NOTATION '->'
-void NoteGenerator::setNote(double base_freq, string base_note, int num_of_notes) {
+void NoteGenerator::setNote(double base_freq, std::string base_note, int num_of_notes) {
 	char letter=base_note[0];
 	bool has_sharp=false;
 	if (base_note[1] == '#') {
@@ -28,9 +35,10 @@ void NoteGenerator::setNote(double base_freq, string base_note, int num_of_notes
 	int number=has_sharp?base_note[2]:base_note[1]-'0';
 	string note_name=base_note;
 	double freq = base_freq;
-	Note* keyboard = new Note[num_of_notes];
+
 	keyboard[0].note_frequency = freq;
 	keyboard[0].note_name = note_name;
+	keyboard[0].note_duration = 0; // the keyboard doesn't have note durations
 	int octave_count = 0;
 	for(int i = 1; i < num_of_notes; i++){
 		note_name = "";
@@ -73,5 +81,22 @@ void NoteGenerator::setNote(double base_freq, string base_note, int num_of_notes
 		freq *= 1.059463;
 		keyboard[i].note_frequency = freq;
 		keyboard[i].note_name = note_name;
+		keyboard[i].note_duration = 0; // the keyboard doesn't have note durations
 	}
+}
+
+Note NoteGenerator::getRandomNote() 
+{
+	Note rand_note;
+
+//	srand(time(NULL));
+
+	int rand_note_index = rand() % (this->keyboard.size());
+	rand_note.note_frequency = getNoteFreq(rand_note_index);
+	rand_note.note_name = getNoteName(rand_note_index);
+	
+	int rand_note_duration = (rand() % 2) + 1; // notes can be quarter (1 beat) or half (2 beats)
+	rand_note.note_duration = rand_note_duration;
+	
+	return rand_note;
 }
--- headers/note_generator.h
+++ headers/note_generator.h
@@ -1,26 +1,33 @@
 #pragma once
 #include <string>
+#include <vector>
+
 using namespace std;
+
+const size_t default_keyboard_size = 24;
 
 struct Note{
 	double note_frequency;
 	string note_name;
+	int note_duration;
 };
 
 class NoteGenerator {
+private:
+	std::vector<Note>	keyboard;
+
 public:
 	//Constructor
 	 NoteGenerator();
 	//~NoteGenerator(); May or may not use
 
 	//Accessors
-	double      getNoteFreq(int index);
-	string		getNoteName(int index);
-	Note*       getArray();
+	double				getNoteFreq(int index);
+	string				getNoteName(int index);
+	std::vector<Note>	getKeyboard();
+	size_t				getKeyboardSize();
+	Note 				getRandomNote();
 
 	//Mutators
 	void        setNote(double base_freq, string base_note, int num_of_notes);	//starting from the base note with base freq, count numOfNotes notes
-
-private:
-	Note*       keyboard;
 };
