11a4863b042a009467b7087e12bcafa1c995ee9f
shida-yang <shidayang@ufl.edu>
2018-04-19 18:46:38 -0400

    fix note generator

--- classes/note_generator.cpp
+++ classes/note_generator.cpp
@@ -1,15 +1,16 @@
-#include "../headers/note_generator.h"
+#include "note_generator.h"
 #include <algorithm>
 #include <vector>
 #include <ctime> // To be used for note generator
 
 // constructor defaults to the note range starting one octave below middle C (C3) and being 2 octaves
 NoteGenerator::NoteGenerator() {
+	keyboard = std::vector<Note>();
 	NoteGenerator::setNote(130.81, std::string("C3"), default_keyboard_size);
 }
 
 //frequncy getter
-double NoteGenerator::getNoteFreq(int index){
+double NoteGenerator::getNoteFreq(int index) {
 	return keyboard.at(index).note_frequency;
 }
 //name getter
@@ -27,20 +28,20 @@ size_t NoteGenerator::getKeyboardSize() {
 
 //Mutators  //ERROR MIGHT OCCUR DUE TO ME NOT USING POINTER NOTATION '->'
 void NoteGenerator::setNote(double base_freq, std::string base_note, int num_of_notes) {
-	char letter=base_note[0];
-	bool has_sharp=false;
+	char letter = base_note[0];
+	bool has_sharp = false;
 	if (base_note[1] == '#') {
 		has_sharp = true;
 	}
-	int number=has_sharp?base_note[2]:base_note[1]-'0';
-	std::string note_name=base_note;
+	int number = has_sharp ? base_note[2] : base_note[1] - '0';
+	std::string note_name = base_note;
 	double freq = base_freq;
-
+	keyboard.push_back(Note());
 	keyboard[0].note_frequency = freq;
 	keyboard[0].note_name = note_name;
 	keyboard[0].note_duration = 0; // the keyboard doesn't have note durations
 	int octave_count = 0;
-	for(int i = 1; i < num_of_notes; i++){
+	for (int i = 1; i < num_of_notes; i++) {
 		note_name = "";
 		//determine number
 		if (octave_count == 11) {
@@ -79,6 +80,7 @@ void NoteGenerator::setNote(double base_freq, std::string base_note, int num_of_
 		}
 		//calculate frequency
 		freq *= 1.059463;
+		keyboard.push_back(Note());
 		keyboard[i].note_frequency = freq;
 		keyboard[i].note_name = note_name;
 		keyboard[i].note_duration = 0; // the keyboard doesn't have note durations
@@ -88,37 +90,35 @@ void NoteGenerator::setNote(double base_freq, std::string base_note, int num_of_
 Note NoteGenerator::getRandomNote(bool is_major_key) {
 	Note rand_note;
 
-//	srand(time(NULL));
+	//	srand(time(NULL));
 	bool valid_key = false;
 	int rn_i; // rn_i is the random note's index
 	if (is_major_key) {
 		do {
 			rn_i = rand() % (this->keyboard.size());
-			if ((rn_i % 12 == 0) || (rn_i % 12 == 2) || (rn_i % 12 == 4) || (rn_i % 12 == 5) || 
-				(rn_i % 12 == 7) || (rn_i % 12 == 8) || (rn_i % 12 == 10)) 
+			if ((rn_i % 12 == 0) || (rn_i % 12 == 2) || (rn_i % 12 == 4) || (rn_i % 12 == 5) ||
+				(rn_i % 12 == 7) || (rn_i % 12 == 8) || (rn_i % 12 == 10))
 				valid_key = true;
-		}
-		while (valid_key == false);
+		} while (valid_key == false);
 	}
 	else {
 		do {
 			rn_i = rand() % (this->keyboard.size());
-			if ((rn_i % 12 == 0) || (rn_i % 12 == 2) || (rn_i % 12 == 3) || (rn_i % 12 == 5) || 
+			if ((rn_i % 12 == 0) || (rn_i % 12 == 2) || (rn_i % 12 == 3) || (rn_i % 12 == 5) ||
 				(rn_i % 12 == 7) || (rn_i % 12 == 8) || (rn_i % 12 == 10))
 				valid_key = true;
-		}
-		while (valid_key == false);	
+		} while (valid_key == false);
 	}
-	
+
 	rand_note.note_frequency = getNoteFreq(rn_i);
 	rand_note.note_name = getNoteName(rn_i);
-	
+
 	/*
 	int rand_note_duration = (rand() % 2) + 1; // notes can be quarter (1 beat) or half (2 beats)
 	rand_note.note_duration = rand_note_duration;
 	*/
 	rand_note.note_duration = 1; // Melody notes will only last a quarter beat
-	
+
 	return rand_note;
 }
 
@@ -138,8 +138,8 @@ Note NoteGenerator::getRandomNote(bool is_major_key) {
 as high as the keyboard gets
 */
 Note NoteGenerator::getNote(int index) {
-	index %= 7;
 	int multiple = index / 7;
+	index %= 7;
 	switch (index) {
 	case 1:
 		return keyboard.at(0 + multiple * 12);
@@ -153,7 +153,7 @@ Note NoteGenerator::getNote(int index) {
 		return keyboard.at(7 + multiple * 12);
 	case 6:
 		return keyboard.at(9 + multiple * 12);
-	case 7:
+	case 0:
 		return keyboard.at(11 + multiple * 12);
 	}
 
