bd1bfb6e9c9383d39e02ed18f8c025d4480079b8
Logan <loganda.smith@ufl.edu>
2018-04-20 12:10:02 -0400

    SongWriter Cleanup

--- classes/song_writer.cpp
+++ classes/song_writer.cpp
@@ -22,11 +22,10 @@ int progressions[Num_of_Progressions][4] =
 
 
 //Constructor
-SongWriter::SongWriter(int tempo, int num_measures, std::string mood)
+SongWriter::SongWriter(int tempo, int num_measures)
 {
 	SongWriter::setTempo(tempo);
 	SongWriter::setNumMeasures(num_measures);
-	SongWriter::setMood(mood);
 	myChords = new ChordGenerator;
 	note_gen = new NoteGenerator;
 	vector<Chord*> song();
@@ -36,11 +35,6 @@ SongWriter::SongWriter(int tempo, int num_measures, std::string mood)
 int SongWriter::getTempo()
 {
 	return this->tempo;
-}
-
-std::string SongWriter::getMood()
-{
-	return this->mood;
 }
 
 Chord SongWriter::getChord(int index)
@@ -62,11 +56,6 @@ void SongWriter::setTempo(int tempo)
 void SongWriter::setNumMeasures(int num_measures)
 {
 	this->num_measures = num_measures;
-}
-
-void SongWriter::setMood(std::string mood)
-{
-	this->mood  = mood;
 }
 
 void SongWriter::addChord(Chord new_chord)
--- headers/song_writer.h
+++ headers/song_writer.h
@@ -21,39 +21,36 @@ class SongWriter
 {
 public:
 	//Constructor
-	SongWriter(int tempo, int num_measures, std::string mood);
+	SongWriter(int tempo, int num_measures);
 
 	//Accessors
-	int					getTempo();
-	std::string			getMood();
-	Chord				getChord(int index);
-	int					findChord(string chord_name);
-	std::vector<Chord*[4]>	getSong();
-	int*        getHarmony();
-	float*      getMelody(); //gets compatible melody notes with chords in a progression
+	int		         getTempo();
+	Chord	         getChord(int index);
+	int			     findChord(string chord_name);
+	std::vector<Chord*[4]> getSong();
+	int*             getHarmony();
+	float*           getMelody(); //gets compatible melody notes with chords in a progression
 
 	//Mutators
-	void               setTempo(int tempo);
-	void               setNumMeasures(int num_measures);
-	void               setMood(std::string mood);
-	void               addChord(Chord new_chord);
-	void               removeChord(int index);
-	void			   			 generateSong();
-	void               printSong();
-	void               clearSong();
-	void 							 writeSong();
+	void             setTempo(int tempo);
+	void             setNumMeasures(int num_measures);
+	void             addChord(Chord new_chord);
+	void             removeChord(int index);
+	void    	     generateSong();
+	void             printSong();
+	void             clearSong();
+	void             writeSong();
 
 private:
-	const int          SONG_LENGTH = 16; //# of chords / num_measures
-	const int 				 NUM_MELODY_NOTES = 4 * SONG_LENGTH;
-	NoteGenerator*     note_gen;
-	int                tempo;
-	int                num_measures;
-	std::string        mood;
+	const int        SONG_LENGTH = 16; //# of chords / num_measures
+	const int        NUM_MELODY_NOTES = 4 * SONG_LENGTH;
+	NoteGenerator*   note_gen;
+	int              tempo;
+	int              num_measures;
 	std::vector<Chord*[4]> song; //create song_harmony & song_melody vectors?
 	//std::vector<Note> song_melody; //store all notes in a melody vector
 	//Note[8]             melody; //stores 8 notes compatible with at least 2 chords; 8 melody half-notes per progression
-	ChordGenerator*     myChords; //NEW* 4/2/18 - Jared L.
+	ChordGenerator*  myChords; //NEW* 4/2/18 - Jared L.
 	std::vector<int> harmony;
   std::vector<float> melody;
 };
