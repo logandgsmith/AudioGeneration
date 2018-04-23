81dc2433766e6c530f442e17d85136e5fb096731
Damien N Bobrek <dbobrek@ufl.edu>
2018-04-22 20:15:36 -0400

    Note generator documentation changes

--- classes/note_generator.cpp
+++ classes/note_generator.cpp
@@ -3,6 +3,8 @@
 #include <ctime> // To be used for note generator
 #include <stdexcept>
 
+using namespace std;
+
 /**************************************************
 This class defines a struct called Note, which
 contains the note name and note frequency. The
@@ -12,16 +14,10 @@ These notes can be used to form chords and melody
 in our song.
 ***************************************************/
 
-/*
-* Triple slashes ("///") denote a location where the note_duration value
-* of the struct Note was used. Currently, there is no apparent need for
-* the duration of notes to be stored within their respective struct
-*/
-
-// constructor defaults to the note range starting one octave below middle C (C3) and being 2 octaves
+// constructor defaults to the note range starting one octave below middle C (C3) and being 3 octaves
 NoteGenerator::NoteGenerator() {
-	keyboard = std::vector<Note>();
-	NoteGenerator::setNote(130.81, std::string("C3"), default_keyboard_size);
+	keyboard = vector<Note>();
+	NoteGenerator::setNote(default_base_freq, default_base_note_name, default_keyboard_size);
 }
 
 //frequncy getter
@@ -30,10 +26,12 @@ float NoteGenerator::getNoteFreq(unsigned char index) {
 }
 
 //name getter
-std::string NoteGenerator::getNoteName(unsigned char index) {
+string NoteGenerator::getNoteName(unsigned char index) {
 	return getNote(index).note_name;
 }
 
+//Finds a note's index given a note name (ex. C4)
+//It will return 255 if the note does not exist on the keyboard.
 unsigned char NoteGenerator::getNoteIndex(string note_name) {
 	for (int i = 0; i < getKeyboardSize(); i++) {
 		string curr_name = keyboard.at(i).note_name;
@@ -45,14 +43,19 @@ unsigned char NoteGenerator::getNoteIndex(string note_name) {
 }
 
 //keyboard vector getter
-std::vector<Note> NoteGenerator::getKeyboard() {
+//It will return the entire keyboard vector.
+vector<Note> NoteGenerator::getKeyboard() {
 	return this->keyboard;
 }
 //keyboard size getter
+//It will return the keyboard's size, which can be up to size 254
+// (It cannot ve size 255, as the getNoteIndex function returns 255 if it cannot find a number.
 unsigned char NoteGenerator::getKeyboardSize() {
 	return this->keyboard.size();
 }
-//keyboard size without sharps
+//keyboard size without notes outside of the actual key (ex. in C major it is the number of white keys)
+//Currently, it only works correctly if the keyboard does not have a range that includes something outside of just octaves
+//	(ex. a keyboard 3.5 octaves (42 notes) in size will only return saying it has 21 notes instead of more, as it should.)
 unsigned char NoteGenerator::getMajorKeyboardSize() {
 	unsigned char actual_keyboard_size = getKeyboardSize();
 	unsigned char major_keyboard_size = actual_keyboard_size / 12 * 7;
@@ -60,15 +63,18 @@ unsigned char NoteGenerator::getMajorKeyboardSize() {
 }
 
 //Mutators
-//set all the notes in the keyboard. give each note a note name, frequency, and duration 0
-void NoteGenerator::setNote(float base_freq, std::string base_note, unsigned char num_of_notes) {
+//Generates the keyboard. It will give each note in the specified num_of_notes range a name, frequency, and duration 0.
+//It erases the previous keyboard vector contents. (Of course, if there weren't any this won't actually do anything.)
+void NoteGenerator::setNote(float base_freq, string base_note, unsigned char num_of_notes) {
+	keyboard.clear();
+
 	char letter = base_note[0];
 	bool has_sharp = false;
 	if (base_note[1] == '#') {
 		has_sharp = true;
 	}
 	int number = has_sharp ? base_note[2] : base_note[1] - '0';
-	std::string note_name = base_note;
+	string note_name = base_note;
 	float freq = base_freq;
 	keyboard.push_back(Note());
 	keyboard[0].note_frequency = freq;
@@ -134,20 +140,20 @@ unsigned char NoteGenerator::getRandomNote() {
 
 //get note based on the index on the major scale (after removing all the sharps)
 /*
-1=C
-2=D
-3=E
-4=F
-5=G
-6=A
-7=B
-8=C
+Since by default our bottom note is C3,
+1=C3
+2=D3
+3=E3
+4=F3
+5=G3
+6=A3
+7=B3
+8=C4
 .
 .
 .
 as high as the keyboard gets
 */
-
 Note NoteGenerator::getNote(unsigned char index) 
 {
 	unsigned char multiple = (index - 1) / 7;
@@ -170,10 +176,25 @@ Note NoteGenerator::getNote(unsigned char index)
 	case 0:
 		return keyboard.at(11 + multiple * 12);
 	default:
-		throw std::domain_error("Invalid note index");
+		throw domain_error("Invalid note index");
 	}
 }
 
+//gets the notes that are not within the key of the keyboard (by default it is C major)
+/*
+Since by default our bottom note is C3,
+1=C#3
+2=D#3
+3=F#3
+4=G#3
+5=A#3
+6=C#4
+7=D#4
+.
+.
+.
+as high as the keyboard gets
+*/
 Note NoteGenerator::getSharpNote(unsigned char index) 
 {
 	unsigned char multiple = (index - 1) / 5;
@@ -190,6 +211,6 @@ Note NoteGenerator::getSharpNote(unsigned char index)
 	case 0:
 		return keyboard.at(10 + multiple * 12);
 	default:
-		throw std::domain_error("Invalid note index");
+		throw domain_error("Invalid note index");
 	}
 }
